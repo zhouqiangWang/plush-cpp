4f71e8ecc4e76b13808577cd0d3660e62903a45e
Joe Wang <zhouqiang.wang@hotmail.com>
2020-02-16 23:00:44 -0800

    Add stdc++.h and unordered_map example

--- Hello.cpp
+++ Hello.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include "bits/stdc++.h"
 
 using namespace std;
 
 int main() {
     cout<<"hello world !"<<endl;
+
+    unordered_map<int, string> umap;
+    umap[1] = "hello";
+    umap[2] = "from";
+    umap[3] = "outside";
+    cout << umap.size() << endl;
+    
+    for(auto i : umap) {
+        cout << i.first << " - " << i.second << endl;
+    }
+
+    auto begin = umap.begin();
+
+    cout << begin->first << " - " << begin->second << endl;
+
     return 0;
 }
